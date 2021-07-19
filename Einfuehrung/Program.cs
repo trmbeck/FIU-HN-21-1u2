bef9247a45ca5b7581fceb39fe696c54a860f73f
Michael Beck <mbeck@lutz-und-grub.de>
2021-07-19 15:52:46 +0200

    Schleifen

--- Einfuehrung/Program.cs
+++ Einfuehrung/Program.cs
@@ -7,22 +7,58 @@ namespace Einfuehrung
         static void Main(string[] args)
         {
             //Start
-            int kapital;
-            Console.WriteLine("Bitte Kapital eingeben");
-            string eingabe = Console.ReadLine();
-            kapital = Convert.ToInt32(eingabe);
-            double zinsen = kapital * 0.03;
-            Console.WriteLine("Ihre Zinsen betragen: {0:f2}",zinsen);
-            Console.WriteLine($"Ihre Zinsen betragen: {zinsen}");
+            int kapital = 0;
+            bool zahlKorrekt = false;
+            bool nochmal = true;
 
-            //Escape-Sequenz -> Einleitung durch \
-            Console.WriteLine("Hallo\rWelt\a");
-            Console.WriteLine("\\\\Server01\\freigabename\\ordner");// -> \Server01reigabename
-            //Verbatim-String
-            Console.WriteLine(@"\\Server01\Freigabename\ordner");
-            Console.ReadLine();
+
+            while (nochmal)
+            {
+                Console.Clear(); 
+                do
+                {
+                    Console.WriteLine("Bitte Kapital eingeben");
+                    string eingabe = Console.ReadLine();
+                    try
+                    {
+                        kapital = Convert.ToInt32(eingabe);
+                        zahlKorrekt = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Falsches Format:");
+                    }
+                } while (!zahlKorrekt);
+                zahlKorrekt = false;
+
+                double zinsen;
+                if (kapital > 10000)
+                {
+                    zinsen = kapital * 0.04;
+                }
+                else
+                {
+                    zinsen = kapital * 0.03;
+                }
+
+
+                Console.WriteLine("Ihre Zinsen betragen: {0:f2}", zinsen);
+                Console.WriteLine($"Ihre Zinsen betragen: {zinsen}");
+
+                goto EscapeSequenzFertig;
+                //Escape-Sequenz -> Einleitung durch \
+                Console.WriteLine("Hallo\rWelt\a");
+                Console.WriteLine("\\\\Server01\\freigabename\\ordner");// -> \Server01reigabename
+                                                                        //Verbatim-String
+                Console.WriteLine(@"\\Server01\Freigabename\ordner");
+                Console.ReadLine();
             //Fertig
             //weitere Version
+            EscapeSequenzFertig:
+                Console.WriteLine("Nochmal (J/N)?");
+                if (Console.ReadLine().ToUpper() != "J") nochmal = false;
+                
+            }
         }
     }
 }

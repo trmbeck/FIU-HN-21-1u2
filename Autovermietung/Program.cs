a827c2d721daa7bea679df1eff17c9478184adc7
trmbeck <michael.beck@training.lug-ag.de>
2021-07-21 14:10:45 +0200

    Übungen 1 - 3

---
+++ Autovermietung/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Autovermietung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Entwerfen (PAP oder Struktogramm) und implementieren (C# Code) Sie ein Rechnungsprogramm für einen Automvermieter. Wenn ein Kunde den Normaltarif (68€) auswählt, sind 200 km Fahrtstrecke inkludiert. Erst wenn der Kunde mehr als 200 km fährt, muss er 65 Cent pro weiteren Kilometer zahlen. Die Preise sind exkl. Mehrwertsteuer.
+
+                Eingabe: gefahrene Kilometer
+
+                Ausgabe: Rechnungsbetrag inkl. MwSt., MwSt. als Betrag
+             */
+
+            double mwstSatz = 0.19;
+            double zuschlag = 0.65;
+            double normaltarif = 68;
+            double kilometerFrei = 200;
+            double nettobetrag;
+            double kilometer;
+            double mwstBetrag;
+
+            Console.Write("gefahrene Kilometer: ");
+            string eingabe = Console.ReadLine();
+            kilometer = Convert.ToDouble(eingabe);
+
+            if (kilometer > 200)
+            {
+                //Aufschlag
+                nettobetrag = normaltarif + (kilometer - kilometerFrei) * zuschlag;
+            }
+            else
+            {
+                //Normaltarif
+                nettobetrag = normaltarif;
+            }
+
+            mwstBetrag = nettobetrag * mwstSatz;
+
+
+            Console.WriteLine("Rechnungsbetrag: {0:f2}", nettobetrag + mwstBetrag);
+            Console.WriteLine("enthaltene MwSt: {0:f2}", mwstBetrag);
+
+            Console.WriteLine("Bitte Taste drücken ...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Durchschnitt/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Durchschnitt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Schreiben Sie ein Programm, welches fünf Zahlen abfrägt und 
+                danach den Durchschnitt dieser Zahlen berechnet und ausgibt.
+
+                Erweitern Sie das Programm so, dass die Abfrage der Zahlen vorzeitig 
+                beenden werden kann, wenn der Buchstabe "A" eingegeben wird. 
+                Falls die fünfte Zahl eingegeben wurde beenden Sie ebenfalls die Abfrage. 
+                Berechnen Sie aus den eingegebenen Zahlen den Durchschnitt und 
+                geben Sie ihn aus. 
+             */
+            int summe = 0;
+            int i;
+            for (i = 0; i < 5; i = i + 1)
+            {
+                string eingabe = Console.ReadLine();
+                if (eingabe == "a")
+                {
+                    //abbrechen
+                    break;
+                }
+                else
+                {
+                    summe = summe + int.Parse(eingabe);
+                }
+            }
+            
+            //weiter nach der Schleife
+            Console.WriteLine("Durchschnitt: " + (summe/(double)i));
+
+
+        }
+    }
+}

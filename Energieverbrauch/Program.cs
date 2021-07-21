d71009e140c95c5f61571f45b36cb90c50ce8829
trmbeck <michael.beck@training.lug-ag.de>
2021-07-21 16:18:23 +0200

    Energieverbrauch passwort

---
+++ Energieverbrauch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Energieverbrauch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Teil 1:
+             Erstellen Sie eine neue Konsolenanwendung namens "Energieverbrauch".
+             Definieren Sie die Variablen, um den Energieverbrauch eines elektrischen Geräts für ein Jahr zu berechnen.
+             Berechnung: Verbrauch = Elektrische Leistung x Stunden je Tag x Tage pro Jahr
+
+             Teil 2:
+             Implementieren Sie folgende Ein- und Ausgaben
+             Eingaben: elektrische Leistung, Stunden je Tag, Tage pro Jahr
+             Ausgaben: elektrische Leistung 
+             */
+
+            //Deklaration und Initialisierung von Variablen
+            int leistung, stunden, tage = 300; // die Werte nennt man Literale
+
+            //Eingabe
+            Console.Write("Leistung: ");
+            string eingabe = Console.ReadLine();
+            leistung = Convert.ToInt32(eingabe);
+
+            Console.Write("Stunden: ");
+            eingabe = Console.ReadLine();
+            stunden = Convert.ToInt32(eingabe);
+
+            Console.Write("Tage: ");
+            eingabe = Console.ReadLine();
+            tage = Convert.ToInt32(eingabe);
+
+            int verbrauch;
+
+            //Verarbeitung
+            verbrauch = leistung * stunden * tage;
+
+            //Ausgabe
+            Console.WriteLine("Verbrauch: " + verbrauch + "Wh");
+
+            //Obligatorisches Anhalten
+            Console.WriteLine("bitte Taste drücken zum beenden ...");
+            Console.ReadKey();
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ Passwort/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Passwort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eingabe;
+            int zaehler = 1;
+            bool P = false, R = false, O = false, G = false;
+            int anzahl = 1;
+
+            do
+            {
+                do
+                {
+                    Console.WriteLine($"bitte {zaehler}. Zeichen eingeben:");
+                    eingabe = Console.ReadLine();
+
+                    if (eingabe.ToUpper() == "P") P = true;
+                    if (eingabe.ToUpper() == "R") R = true;
+                    if (eingabe.ToUpper() == "O") O = true;
+                    if (eingabe.ToUpper() == "G") G = true;
+
+                    #region Lösung mit switch
+                    switch (eingabe.ToUpper())
+                    {
+                        case "P":
+                            P = true;
+                            break;
+                        case "R":
+                            R = true;
+                            break;
+                        case "O":
+                            O = true;
+                            break;
+                        case "G":
+                            G = true;
+                            break;
+                    }
+                    #endregion
+
+                    zaehler++;
+                } while (zaehler <= 4);
+                
+                zaehler = 1;
+                if (P && R && O && G)
+                {
+                    //passwort korrekt!!!
+                    Console.WriteLine("korrekt");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("falsch");
+                }
+                anzahl++;
+            } while (anzahl <= 3);
+        }
+    }
+}

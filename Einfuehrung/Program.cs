6c2fe80cf0785999b3deb96e184c79ed989a75e1
trmbeck <michael.beck@training.lug-ag.de>
2021-07-22 11:57:45 +0200

    Switch und Methoden

--- Einfuehrung/Program.cs
+++ Einfuehrung/Program.cs
@@ -11,14 +11,95 @@ namespace Einfuehrung
             bool zahlKorrekt = false;
             bool nochmal = true;
 
+            bool wiederholen = false;
+            string eingabe;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("a) Kapital berechnen");
+                Console.WriteLine("b) Finanzierung berechnen");
+                Console.WriteLine("c) Aktiendividende berechnen");
+                Console.WriteLine("d) Beenden");
+
+                Console.Write("Bitte wählen: ");
+                eingabe = Console.ReadLine();
+
+                //if (eingabe == "1") {/*Kapital berechnen*/}
+                //if (eingabe == "2") {/*Finanzierung berechnen*/}
+                //if (eingabe == "3") {/*Aktiendividende berechnen*/}
+                //if (eingabe == "4") {/*Beenden*/}
+
+
+                #region mehrseitige Auswahl
+                if (eingabe == "1")
+                {
+                    //Kapital berechnen
+                }
+                else
+                {
+                    if (eingabe == "2")
+                    {
+                        //Finanzierung berechnen
+                    }
+                    else
+                    {
+                        if (eingabe == "3")
+                        {
+                            //Aktiendividende berechnen
+                        }
+                        else
+                        {
+                            if (eingabe == "4")
+                            {
+                                //Beenden
+                            }
+                            else
+                            {
+                                // Falsche Eingabe
+                            }
+                        }
+                    }
+                }
+                #endregion
+
+
+                wiederholen = false;
+                //Fallauswahl
+                switch (eingabe)
+                {
+                    case "A":
+                    case "a":
+                        // Kapitalberechnen
+                        Console.WriteLine("Das Kapital wurde berechnet");
+                        break;
+                    case "b":
+                        Console.WriteLine("Die Finanzierung wrude berechnet");
+                        break;
+                    case "c":
+                        Console.WriteLine("Aktiendividende wurde berechnet");
+                        break;
+                    case "d":
+                        Console.WriteLine("Beenden");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Falscheingabe");
+                        wiederholen = true;
+                        break;
+                }
+            }
+            while (wiederholen);
+
+            Console.ReadLine();
 
             while (nochmal)
             {
-                Console.Clear(); 
+                //Console.Clear();
                 do
                 {
                     Console.WriteLine("Bitte Kapital eingeben");
-                    string eingabe = Console.ReadLine();
+                    eingabe = Console.ReadLine();
                     try
                     {
                         kapital = Convert.ToInt32(eingabe);
@@ -45,8 +126,8 @@ namespace Einfuehrung
                 Console.WriteLine("Ihre Zinsen betragen: {0:f2}", zinsen);
                 Console.WriteLine($"Ihre Zinsen betragen: {zinsen}");
 
-                goto EscapeSequenzFertig;
-                //Escape-Sequenz -> Einleitung durch \
+                goto EscapeSequenzFertig; //goto ist böse!!!
+                                          //Escape-Sequenz -> Einleitung durch \
                 Console.WriteLine("Hallo\rWelt\a");
                 Console.WriteLine("\\\\Server01\\freigabename\\ordner");// -> \Server01reigabename
                                                                         //Verbatim-String
@@ -57,8 +138,13 @@ namespace Einfuehrung
             EscapeSequenzFertig:
                 Console.WriteLine("Nochmal (J/N)?");
                 if (Console.ReadLine().ToUpper() != "J") nochmal = false;
-                
+
             }
+
+
+
+
+
         }
     }
 }
---
+++ Methoden/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace Methoden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            string eingabe;
+            //Menü
+            do
+            {
+                Unterprogramm();
+                eingabe = Console.ReadLine(); //Eingabe eines Menüpunktes
+                Console.Clear();
+                switch (eingabe)
+                {
+                    case "1":
+                        PrimzahlenBis50();
+                        break;
+                    case "2":
+                        double nummer = DoubleZahlEingeben();
+                        double invNummer = Invertieren(nummer);
+                        break;
+                    case "3":
+                        double[] nummern = ZahlenEingeben(2);
+                        double s = Addieren(nummern[0], nummern[1]);
+                        Console.WriteLine(s);
+                        break;
+                    case "4":
+                        Subtrahieren();
+                        break;
+                    case "6":
+                        ZahlenEingeben(10);
+                        break;
+                }
+
+                Console.ReadLine();
+            } while (eingabe != "5");
+
+
+
+
+        }
+
+
+        static double[] ZahlenEingeben(int anzahl)
+        {
+            double[] zahlen = new double[anzahl];
+            for (int i = 0; i < anzahl; i++)
+            {
+                Console.Write("Zahl {0} eingeben: ", i + 1);
+                zahlen[i] = double.Parse(Console.ReadLine());
+            }
+            return zahlen;
+        }
+
+        static double Addieren(double z1, double z2)
+        {
+            double summe = z1 + z2;
+            return summe;
+        }
+
+        static double DoubleZahlEingeben()
+        {
+            Console.WriteLine("Zahl eingeben: ");
+            double zahl = double.Parse(Console.ReadLine());
+            return zahl;
+        }
+
+        static void Subtrahieren()
+        {
+            Console.WriteLine("Zahl 1 eingeben: ");
+            double zahl1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Zahl 2 eingeben: ");
+            double zahl2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Differenz: " + (zahl1 - zahl2));
+        }
+
+        static double Invertieren(double zuInvertierendeZahl)
+        {
+            double InvertierteZahl = zuInvertierendeZahl * -1;
+            Console.WriteLine("Invertiert: " + InvertierteZahl);
+            return InvertierteZahl;
+        }
+
+        static void PrimzahlenBis50()
+        {
+            Console.WriteLine("Primzahlen bis 50:");
+            Console.WriteLine("2\t3\t5\t7\t11\t13\t17\t19\t23\t29\t31\t37\t41\t47");
+        }
+
+
+        static void Unterprogramm()
+        {
+            Console.Clear();
+            Console.WriteLine("1. Primzahlen bis 50");
+            Console.WriteLine("2. Invertieren");
+            Console.WriteLine("3. Addieren");
+            Console.WriteLine("4. Subtrahieren");
+            Console.WriteLine("5. Beenden");
+            Console.WriteLine("6. Untermenue");
+            Console.WriteLine("Ihre Wahl: ");
+        }
+
+        static void Untermenue()
+        {
+            Console.WriteLine("1. bla");
+            Console.WriteLine("2. fasel");
+            Console.WriteLine("3. zurück");
+        }
+
+    }
+}

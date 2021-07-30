d0b55d45a24264c6e46132dde9855455d2b3b506
trmbeck <michael.beck@training.lug-ag.de>
2021-07-30 10:05:53 +0200

    statische Klassen, Variablen, Methoden

---
+++ StatischeKlassen/Mathe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatischeKlassen
+{
+    class Mathe
+    {
+        public double wert; //Objektvariable
+        public const double PI = 3.14; //Klassenvariable
+        public static readonly double pi = 3.14; // nicht mehr änderbar (readonly)
+        public static double Summe(params double[] summanden) //Klassenmethode
+        {
+            double result = 0;
+            foreach (var sumand in summanden)
+            {
+                result += sumand;
+            }
+            return result;
+        }
+
+        public double Betrag() //Objektmethode
+        {
+            if (wert < 0) return wert * -1;
+            else return wert;
+        }
+        public static double Betrag(double betrag) //Klassenmethode
+        {
+            if (betrag < 0) return betrag * -1;
+            else return betrag;
+        }
+    }
+}
---
+++ StatischeKlassen/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StatischeKlassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathe m = new Mathe();
+            Mathe.Summe(9, 3, 6, 2, 4.3, 6.7);
+            Console.WriteLine(Mathe.PI);//m.PI = 1.3;
+            //Mathe n = new Mathe(); 
+            Console.WriteLine(Mathe.PI);
+            Mathe.Summe(2, 4, 6, 8, 1, 3);
+            m.wert = -30;
+            Console.WriteLine(m.Betrag());
+            Console.WriteLine(Mathe.Betrag(-254354));
+        }
+    }
+}
